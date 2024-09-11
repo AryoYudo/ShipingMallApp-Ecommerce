4b823d9f79922c9c3f7d1e41865668817d4a1075
AryoYudo <aryoyudho29@gmail.com>
2024-09-11 22:29:13 +0700

    Push DropDow

---
+++ src/components/countryDropdown/index_DropDown.js
@@ -0,0 +1,14 @@
+import { FaAngleDown } from "react-icons/fa6";
+
+const dropDown = () => {
+    return(
+        <>
+            <div className='info d-flex flex-colum'>
+                <span>Your Location</span>
+                <span>Batam</span>
+            </div>
+        </>
+    )
+}
+
+export default dropDown;
