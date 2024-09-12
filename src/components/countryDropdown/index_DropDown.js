6d28198c89efd5b7dae7c2d5d7a5523a34bfbcf1
AryoYudo <aryoyudho29@gmail.com>
2024-09-12 22:06:47 +0700

    initial

--- src/components/countryDropdown/index_DropDown.js
+++ src/components/countryDropdown/index_DropDown.js
@@ -4,16 +4,17 @@ import Button  from  '@mui/material/Button';
 const CountryDrop = () => {
     return(
         <>
-            <button type="button" className="btn btn-outline-light countryDrop" style={{ color: '#FF5722', borderColor: '#FF5722' }}>
-                <div className='info d-flex flex-column'>
-                    <span className="label">Your Location</span>
-                    <span className="name">Batam</span>
+<button type="button" className="btn btn-outline-light countryDrop" style={{ color: '#FF5722', border: '1px solid #0' }}>
+                <div className='d-flex justify-content-between align-items-center'>
+                    <div className="d-flex flex-column">
+                        <span className="label">Your Location</span>
+                        <span className="name font-weight-bold">Batam</span>
+                    </div>
+                    <span className="icon" style={{display: 'flex', alignItems: 'center' }}>
+                        <FaAngleDown />
+                    </span>
                 </div>
-                <span className="ml-auto">
-                <FaAngleDown />
-                </span>
             </button>
-
         </>
     )
 }
