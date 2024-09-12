0541d571154e8ecbe32a2e1b2d097294a66de794
AryoYudo <aryoyudho29@gmail.com>
2024-09-12 22:21:14 +0700

    add css

--- src/components/countryDropdown/index_DropDown.js
+++ src/components/countryDropdown/index_DropDown.js
@@ -4,7 +4,7 @@ import Button  from  '@mui/material/Button';
 const CountryDrop = () => {
     return(
         <>
-<button type="button" className="btn btn-outline-light countryDrop" style={{ color: '#FF5722', border: '1px solid #0' }}>
+<button type="button" className="btn btn-outline-light countryDrop" style={{ color: '#FF5722', border: '1px solid rgba(0, 0, 0, 0.5)' }}>
                 <div className='d-flex justify-content-between align-items-center'>
                     <div className="d-flex flex-column">
                         <span className="label">Your Location</span>
